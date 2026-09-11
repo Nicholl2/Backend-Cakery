5edf9e60b23cdcc678ffd416c2c1cd249e1b8421
Nicholl2 <nikolasanindya0@gmail.com>
2026-09-12 01:05:32 +0700

    Fix: Refund Test 2

--- scratch.py
+++
@@ -1,4 +0,0 @@
-from app.utils.phone import normalize_phone
-print(normalize_phone("08123456789", as_http_exception=False))
-print(normalize_phone("+628987654321", as_http_exception=False))
-print(normalize_phone("08111111111", as_http_exception=False))
